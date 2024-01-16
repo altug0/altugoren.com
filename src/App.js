74a6a6a2ae2c89a7edb1289300ca2e2a3f3350fe
altug0 <0@altugoren.dev>
2024-01-16 13:19:53 +0300

    sync with figma

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBarHeader2 from './ui-components/NavBarHeader2';
+import HeroLayout1 from './ui-components/HeroLayout1';
+import MarketingPricing from './ui-components/MarketingPricing';
+import Features2x2 from './ui-components/Features2x2';
+import CTASection from './ui-components/CTASection';
+import MarketingFooter from './ui-components/MarketingFooter';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center'
+    }}>
+      <div style={{
+        display: 'flex',
+        width: '1440px',
+        flexDirection: 'column',
+        alignItems: 'flex-start'
+      }}>
+        <NavBarHeader2 />
+        <HeroLayout1 />
+        <MarketingPricing />
+        <Features2x2 />
+        <CTASection />
+        <MarketingFooter />
+      </div>
     </div>
   );
 }
